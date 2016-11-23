ebab385a1a590ea90f94488ce114008fc0452a65
imxiz <imxiz@DESKTOP-SKJOL5P>
2016-11-23 22:11:44 +1300

    implemented receipt card and bug fix in the register section

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -225,21 +225,40 @@ namespace Contoso_Bank
                     string address = userData.GetProperty<string>("address");
                     string phone = userData.GetProperty<string>("phone");
 
-                    Activity replyToConversation = activity.CreateReply($"Please confirm the following:");
-                    await connector.Conversations.SendToConversationAsync(replyToConversation);
-                    replyToConversation = activity.CreateReply($"Username: {userName}");
-                    await connector.Conversations.SendToConversationAsync(replyToConversation);
-                    replyToConversation = activity.CreateReply($"Password: {password}");
-                    await connector.Conversations.SendToConversationAsync(replyToConversation);
-                    replyToConversation = activity.CreateReply($"Address: {address}");
-                    await connector.Conversations.SendToConversationAsync(replyToConversation);
-                    replyToConversation = activity.CreateReply($"Phone: {phone}");
-                    await connector.Conversations.SendToConversationAsync(replyToConversation);
-
-                    replyToConversation = activity.CreateReply();
+                    Activity replyToConversation = activity.CreateReply();
                     replyToConversation.Recipient = activity.From;
                     replyToConversation.Type = "message";
                     replyToConversation.Attachments = new List<Attachment>();
+
+                    List<ReceiptItem> receiptList = new List<ReceiptItem>();
+
+                    ReceiptItem usernameItem = new ReceiptItem()
+                    {
+                        Title = "Username",
+                        Price = userName
+                    };
+                    receiptList.Add(usernameItem);
+
+                    ReceiptItem passwordItem = new ReceiptItem()
+                    {
+                        Title = "Password",
+                        Price = password
+                    };
+                    receiptList.Add(passwordItem);
+
+                    ReceiptItem addressItem = new ReceiptItem()
+                    {
+                        Title = "Address",
+                        Price = address
+                    };
+                    receiptList.Add(addressItem);
+
+                    ReceiptItem phoneItem = new ReceiptItem()
+                    {
+                        Title = "Phone number",
+                        Price = phone
+                    };
+                    receiptList.Add(phoneItem);
 
                     List<CardAction> cardButtons = new List<CardAction>();
                     CardAction proceedButton = new CardAction()
@@ -266,14 +285,17 @@ namespace Contoso_Bank
                     };
                     cardButtons.Add(cancelButton);
 
-                    SigninCard plCard = new SigninCard()
+                    ReceiptCard plCard = new ReceiptCard()
                     {
+                        Title = "Please confirm the following",
+                        Items = receiptList,
                         Buttons = cardButtons
+
                     };
 
                     Attachment plAttachment = plCard.ToAttachment();
                     replyToConversation.Attachments.Add(plAttachment);
-                    await stateClient.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, userData);
+                    
                     await connector.Conversations.SendToConversationAsync(replyToConversation);
 
                     return Request.CreateResponse(HttpStatusCode.OK);
